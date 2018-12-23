c86d301a64c5f505bf3ef6e674ea45e06afd67f3
runoob <test@runoob.com>
2018-12-23 16:22:33 +0800

    Initial commit

---
+++ src/com/main/java/Test.java
@@ -0,0 +1,10 @@
+package com.main.java;
+//123
+public class Test {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+    public void  get(){
+
+    }
+}
---
+++ src/com/main/java/Test02.java
@@ -0,0 +1,25 @@
+package com.main.java;
+
+/**
+ * @author panjin
+ * @date 2018-12-19-22:21
+ */
+public class Test02 {
+    /**
+     *code
+     */
+    private int a;
+    /**
+     *b
+     */
+     private String b;
+
+    public static void main(String[] args) {
+
+    }
+    public void testUpdate(){
+        System.out.println("helloword");
+    }
+}
+
+   
